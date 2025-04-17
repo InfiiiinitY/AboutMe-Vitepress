151e9d3920c36cd71eef9594635805530ac2d5de
Mattheus John SCW EHWT1Q <john.mattheus2@zf.com>
2025-04-17 11:12:48 +0200

    update gitignore

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('vite').UserConfig} */
-export default {
-    build: {
-        rollupOptions: {
-            external: ["primevue/timeline", "primevue/card"],
-        },
-    },
-};
